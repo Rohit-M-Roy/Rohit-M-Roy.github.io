4eac512292bce555312347f7eef8a83fcee855bd
Rohit-M-Roy <mrrohitroy09@gmail.com>
2022-09-10 00:18:33 +0530

    Color scheme decided subject to change

---
+++ index.js
@@ -0,0 +1,5 @@
+
+
+//For Mobile need to create a hamburger menu in the navbar with only contact me button
+
+//For tablet normal navbar but the photo will be in the middle and the content around it.
